5734108cadd9b2c938d82cfbc67a3fc6b99aed7c
ssingularity <1755405701@qq.com>
2019-01-31 01:56:45 +0800

    fix a bug

--- src/main/java/cn/pipipan/meetingroom/meetingroomregister/Controller/Controller.java
+++ src/main/java/cn/pipipan/meetingroom/meetingroomregister/Controller/Controller.java
@@ -2,17 +2,16 @@ package cn.pipipan.meetingroom.meetingroomregister.Controller;
 
 import cn.pipipan.meetingroom.meetingroomregister.Service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class Controller {
     @Autowired
     Service service;
 
-    @PostMapping("")
+    @GetMapping("")
     public void processFile(@RequestParam(name="fileName") String fileName){
         service.doProcess(fileName);
     }
